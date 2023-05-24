06dc6fbc70d6cbb1a7d7e709bc3e417480af0bd0
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2023-05-25 00:10:08 +0300

    deployment file

--- storage/storage-client/src/lib.rs
+++ storage/storage-client/src/lib.rs
@@ -13,7 +13,7 @@ mod tests {
         let client = StorageClient::new(config);
         let file = tokio::fs::File::open("../../Cargo.lock").await.unwrap();
         client
-            .create_small_file(Uuid::new_v4(), "test", file)
+            .create_small_file("test", file)
             .await
             .unwrap()
     }
@@ -24,7 +24,7 @@ mod tests {
         let client = StorageClient::new(config);
         let file = tokio::fs::File::open("../../CargoBig.lock").await.unwrap();
         client
-            .create_large_file(Uuid::new_v4(), "test_big", file)
+            .create_large_file("test_big", file)
             .await
             .unwrap()
     }
@@ -34,7 +34,7 @@ mod tests {
         let config = Config::try_from_file("../../ClientTest.toml").await;
         let client = StorageClient::new(config);
         let data = client
-            .read_small_file_last_version(Uuid::new_v4(), "test")
+            .read_small_file_last_version("test")
             .await
             .unwrap();
 
@@ -46,7 +46,7 @@ mod tests {
         let config = Config::try_from_file("../../ClientTest.toml").await;
         let client = StorageClient::new(config);
         let data = client
-            .read_large_file(Uuid::new_v4(), "test_big")
+            .read_large_file("test_big")
             .await
             .unwrap();
 
@@ -57,7 +57,7 @@ mod tests {
     async fn delete_file_test() {
         let config = Config::try_from_file("../../ClientTest.toml").await;
         let client = StorageClient::new(config);
-        let _ = client.delete_file(Uuid::new_v4(), "test").await.unwrap();
+        let _ = client.delete_file("test").await.unwrap();
     }
 
     #[tokio::test]
@@ -66,13 +66,12 @@ mod tests {
         let client = StorageClient::new(config);
         let file = tokio::fs::File::open("../../Cargo.lock").await.unwrap();
         client
-            .create_small_file(Uuid::new_v4(), "test", file)
+            .create_small_file("test", file)
             .await
             .unwrap();
 
         client
             .add_new_commit_to_small_file(
-                Uuid::new_v4(),
                 "test",
                 &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
             )
@@ -80,11 +79,11 @@ mod tests {
             .unwrap();
 
         let data = client
-            .read_small_file_last_version(Uuid::new_v4(), "test")
+            .read_small_file_last_version( "test")
             .await
             .unwrap();
 
-        let _ = client.delete_file(Uuid::new_v4(), "test").await.unwrap();
+        let _ = client.delete_file("test").await.unwrap();
 
         assert_eq!(16, data.len());
     }
